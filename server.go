0ca0a6eeac1489d401638865eaef30ee68e4cab3
LongLongMorty <2134693323@qq.com>
2026-06-01 13:35:32 +0800

    feat: V2.0用户上线以及广播功能

--- server.go
+++ server.go
@@ -3,22 +3,28 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
-	Ip   string
-	Port int
+	Ip        string
+	Port      int
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+	Message   chan string
 }
 
-// 创建Server接口
+// NewServer 创建Server接口
 func NewServer(ip string, port int) *Server {
 	return &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 }
 
-// 启动服务器的接口
+// Start 启动服务器的接口
 func (s *Server) Start() {
 	// socekt.Listen()
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port))
@@ -29,6 +35,9 @@ func (s *Server) Start() {
 	fmt.Println("服务器启动成功，等待链接...")
 	// close listen socket
 	defer listener.Close()
+
+	// 启动监听Message的goroutine
+	go s.ListenMassage()
 
 	for {
 		// accept
@@ -43,5 +52,32 @@ func (s *Server) Start() {
 
 }
 func (s *Server) Handler(conn net.Conn) {
-	fmt.Println("链接建立成功")
+	//用户上线，将用户加入OnlineMap中
+	user := NewUser(conn)
+	s.mapLock.Lock()
+	s.OnlineMap[user.Name] = user
+	s.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	s.Broadcast(user, "已上线")
+
+	// 阻塞客户端
+	select {}
+
+}
+func (s *Server) Broadcast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	s.Message <- sendMsg
+}
+
+// ListenMassage 监听Message广播消息的goroutine，一旦有消息就发送给全部在线User
+func (s *Server) ListenMassage() {
+	for {
+		msg := <-s.Message
+		s.mapLock.RLock()
+		for _, u := range s.OnlineMap {
+			u.C <- msg
+		}
+		s.mapLock.RUnlock()
+	}
 }
